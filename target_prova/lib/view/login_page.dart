c2422bdb41c48f4773358379ac5812e0cbdb41d7
SimonUzar <simonuzarrc@gmail.com>
2023-11-07 17:02:29 -0300

    Adicionando TextField, botão, texto e cores de fundo

--- target_prova/lib/view/login_page.dart
+++ target_prova/lib/view/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+import '../widgets/custom_text_form_field.dart';
 import 'routes.dart';
 
 class LoginPage extends StatefulWidget {
@@ -13,32 +14,60 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
         decoration: const BoxDecoration(
-            gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [
-            Colors.blue,
-            Colors.red,
-          ],
-        )),
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromRGBO(27, 77, 101, 1),
+              Color.fromRGBO(45, 149, 142, 1),
+            ],
+          ),
+        ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const TextField(),
-            const TextField(),
-            ElevatedButton(
-              onPressed: () =>
-                  Navigator.pushNamed(context, PageRoutes.informationsPage),
-              child: const Text("Entrar"),
+            const Padding(
+              padding: EdgeInsets.only(bottom: 20),
+              child: CustomTextFormField(
+                labelText: 'Usuário',
+                prefixIcon: Icon(Icons.person),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(bottom: 35),
+              child: CustomTextFormField(
+                labelText: 'Senha',
+                obscureText: true,
+                prefixIcon: Icon(Icons.lock),
+              ),
+            ),
+            SizedBox(
+              width: 140,
+              height: 45,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25.0)),
+                  ),
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.green),
+                ),
+                onPressed: () =>
+                    Navigator.pushNamed(context, PageRoutes.informationsPage),
+                child: const Text("Entrar"),
+              ),
             ),
             GestureDetector(
-              onTap: () =>
-                  launchUrl(Uri.parse("https://www.google.com.br/")),
-              child: const Text("Política de Privacidade"),
+              onTap: () => launchUrl(Uri.parse("https://www.google.com.br/")),
+              child: const Text(
+                "Política de Privacidade",
+              ),
             ),
           ],
         ),

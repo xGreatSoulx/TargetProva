8520831a37051cf6551e805c1c95d9cda30fcef3
SimonUzar <simonuzarrc@gmail.com>
2023-11-07 17:03:15 -0300

    Começando InformationsPage

--- target_prova/lib/view/informations_page.dart
+++ target_prova/lib/view/informations_page.dart
@@ -10,6 +10,18 @@ class InformationsPage extends StatefulWidget {
 class _InformationsPageState extends State<InformationsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color.fromRGBO(27, 77, 101, 1),
+            Color.fromRGBO(45, 149, 142, 1),
+          ],
+        ),
+      ),
+      child: ListView(),
+    );
   }
 }

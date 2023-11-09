d8f1c96958bf39c45cb41322f8cf48d1eac795d1
SimonUzar <simonuzarrc@gmail.com>
2023-11-09 12:33:29 -0300

    Adicionando PageRoutes

---
+++ target_prova/lib/routes.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import 'view/informations_page.dart';
+import 'view/login_page.dart';
+
+class PageRoutes {
+  static const String loginPage = '/loginPage';
+  static const String informationsPage = '/informationsPage';
+
+  static buildRoute(RouteSettings settings) {
+    String name = settings.name ?? '';
+    Uri uri = Uri.parse(name);
+
+    switch (uri.path) {
+      case loginPage:
+        return MaterialPageRoute(
+          builder: (context) => const LoginPage(),
+          settings: settings,
+        );
+
+      case informationsPage:
+        return MaterialPageRoute(
+          builder: (context) => const InformationsPage(),
+          settings: settings,
+        );
+
+      default:
+        return MaterialPageRoute(
+          builder: (context) => const LoginPage(),
+          settings: settings,
+        );
+    }
+  }
+}

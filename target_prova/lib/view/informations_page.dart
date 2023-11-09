2ec5f481edbb862c3a72000d636095d318adf0b3
SimonUzar <simonuzarrc@gmail.com>
2023-11-09 12:35:42 -0300

    Começando o card

--- target_prova/lib/view/informations_page.dart
+++ target_prova/lib/view/informations_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import '../widgets/custom_text_form_field.dart';
 
 class InformationsPage extends StatefulWidget {
   const InformationsPage({super.key});
@@ -10,18 +13,51 @@ class InformationsPage extends StatefulWidget {
 class _InformationsPageState extends State<InformationsPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [
-            Color.fromRGBO(27, 77, 101, 1),
-            Color.fromRGBO(45, 149, 142, 1),
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromRGBO(27, 77, 101, 1),
+              Color.fromRGBO(45, 149, 142, 1),
+            ],
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Spacer(),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.80,
+              height: MediaQuery.of(context).size.height * 0.4,
+              color: Colors.white,
+            ),
+            CustomTextFormField(
+              textAlign: TextAlign.center,
+              hintText: 'Digite o seu texto',
+              validator: (value) {
+                if (value == "") {
+                  return 'Campo obrigatório!';
+                }
+
+                return null;
+              },
+            ),
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: GestureDetector(
+                onTap: () => launchUrl(Uri.parse("https://www.google.com.br/")),
+                child: const Text(
+                  "Política de Privacidade",
+                ),
+              ),
+            ),
           ],
         ),
       ),
-      child: ListView(),
     );
   }
 }

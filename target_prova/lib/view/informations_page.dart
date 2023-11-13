9ac915caca0dd01789a0ea739a4fe03bcbbf532f
SimonUzar <simonuzarrc@gmail.com>
2023-11-12 21:05:21 -0300

    Ajustando ações do card principal

--- target_prova/lib/view/informations_page.dart
+++ target_prova/lib/view/informations_page.dart
@@ -11,142 +11,147 @@ class InformationsPage extends StatefulWidget {
 }
 
 class _InformationsPageState extends State<InformationsPage> {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  String currentInformation = '';
   List<String> informations = [
-    'TESTE1',
-    'TESTE2',
-    'TESTE3',
-    'TESTE4',
+    'INFO1',
+    'INFO2',
+    'INFO3',
+    'INFO4',
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Color.fromRGBO(27, 77, 101, 1),
-              Color.fromRGBO(45, 149, 142, 1),
-            ],
+    return Form(
+      key: formKey,
+      child: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color.fromRGBO(27, 77, 101, 1),
+                Color.fromRGBO(45, 149, 142, 1),
+              ],
+            ),
           ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Spacer(),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 32),
-              child: Container(
-                height: MediaQuery.of(context).size.height * 0.4,
-                color: Colors.white,
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Spacer(),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32),
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.4,
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
                       children: [
-                        const Text(
-                          'Texto Digitado 1',
-                          style: TextStyle(color: Colors.black),
-                        ),
-                        const Icon(Icons.edit),
-                        GestureDetector(
-                          onTap: () {
-                            showDialog(
-                              context: context,
-                              builder: (BuildContext context) => AlertDialog(
-                                title: const Text("Removendo..."),
-                                content: const Text(
-                                    "Deseja remover essa informação?"),
-                                actions: [
-                                  TextButton(
-                                    onPressed: () {
-                                      informations.remove('TESTE1');
-                                      Navigator.pop(context);
-                                    },
-                                    child: const Text('Sim'),
-                                  ),
-                                  TextButton(
-                                    onPressed: () => Navigator.pop(context),
-                                    child: const Text('Não'),
-                                  ),
-                                ],
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Spacer(),
+                            const Text(
+                              'Texto Digitado 1',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
                               ),
-                            );
-                          },
-                          child: const Icon(
-                            Icons.cancel,
-                            color: Colors.red,
+                            ),
+                            const Spacer(),
+                            IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.edit,
+                                size: 32,
+                              ),
+                            ),
+                            IconButton(
+                              onPressed: () {
+                                showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) =>
+                                      AlertDialog(
+                                    title: const Text("Removendo..."),
+                                    content: const Text(
+                                        "Deseja remover essa informação?"),
+                                    actions: [
+                                      TextButton(
+                                        onPressed: () {
+                                          informations.remove('INFO1');
+                                          Navigator.pop(context);
+                                        },
+                                        child: const Text('Sim'),
+                                      ),
+                                      TextButton(
+                                        onPressed: () => Navigator.pop(context),
+                                        child: const Text('Não'),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              },
+                              icon: Icon(
+                                Icons.cancel,
+                                color: Colors.red[800],
+                                size: 32,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const Divider(thickness: 0.5),
+                        const Text(
+                          'INFO',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
                       ],
                     ),
-                  ],
+                  ),
                 ),
               ),
-            ),
-            CustomTextFormField(
-              textAlign: TextAlign.center,
-              hintText: 'Digite o seu texto',
-              validator: (value) {
-                if (value == "") {
-                  return 'Campo obrigatório!';
-                }
+              CustomTextFormField(
+                controller: TextEditingController(text: currentInformation),
+                textAlign: TextAlign.center,
+                autofocus: true,
+                hintText: 'Digite o seu texto',
+                validator: (value) {
+                  if (value == "") {
+                    return 'Campo obrigatório!';
+                  }
 
-                return null;
-              },
-            ),
-            const Spacer(),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 20),
-              child: GestureDetector(
-                onTap: () => launchUrl(Uri.parse("https://www.google.com.br/")),
-                child: const Text(
-                  "Política de Privacidade",
+                  return null;
+                },
+                onFieldSubmitted: (value) {
+                  if (formKey.currentState!.validate()) {
+                    informations.add(currentInformation);
+                    currentInformation = '';
+                    setState(() {});
+                  }
+                },
+              ),
+              const Spacer(),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20),
+                child: GestureDetector(
+                  onTap: () =>
+                      launchUrl(Uri.parse("https://www.google.com.br/")),
+                  child: const Text(
+                    "Política de Privacidade",
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
-// Stack(
-//                                   clipBehavior: Clip.none,
-//                                   children: [
-//                                     const Padding(
-//                                       padding: EdgeInsets.all(24.0),
-//                                       child: Row(
-//                                         children: [
-//                                           Padding(
-//                                             padding: EdgeInsets.symmetric(
-//                                                 horizontal: 8.0),
-//                                             child: Icon(
-//                                               Icons.warning_amber_rounded,
-//                                               color: Colors.amber,
-//                                               size: 50,
-//                                             ),
-//                                           ),
-//                                           Text('Atenção'),
-//                                         ],
-//                                       ),
-//                                     ),
-//                                     Align(
-//                                       alignment: Alignment.topRight,
-//                                       child: Padding(
-//                                         padding: const EdgeInsets.symmetric(
-//                                             horizontal: 8.0, vertical: 4.0),
-//                                         child: IconButton(
-//                                           icon: const Icon(Icons.cancel),
-//                                           padding: EdgeInsets.zero,
-//                                           onPressed: () =>
-//                                               Navigator.pop(context),
-//                                         ),
-//                                       ),
-//                                     ),
-//                                   ],
-//                                 ),
-                                

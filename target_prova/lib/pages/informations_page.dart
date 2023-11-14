fdc40c5dd59fdfe35e55c0f6dc51f1a870e382e3
SimonUzar <simonuzarrc@gmail.com>
2023-11-14 19:21:58 -0300

    Mudando para ListView.builder

--- target_prova/lib/pages/informations_page.dart
+++ target_prova/lib/pages/informations_page.dart
@@ -43,136 +43,146 @@ class _InformationsPageState extends State<InformationsPage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const Spacer(),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 32),
-                child: ListView.builder(
-                  itemCount: informations.length,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      height: MediaQuery.of(context).size.height * 0.4,
-                      color: Colors.white,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                const Spacer(),
-                                Text(
-                                  'Texto Digitado $index',
-                                  style: const TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
+              Flexible(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 32),
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    //physics: const NeverScrollableScrollPhysics(),
+                    itemCount: informations.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        height: MediaQuery.of(context).size.height * 0.4,
+                        color: Colors.white,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Spacer(),
+                                  Text(
+                                    'Texto Digitado $index',
+                                    style: const TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                    ),
                                   ),
-                                ),
-                                const Spacer(),
-                                IconButton(
-                                  onPressed: () {
-                                    setState(() {
-                                      currentInformation = informations[index];
-                                      hashCodeCurrentInformation =
-                                          informations[index].hashCode;
-                                    });
-                                  },
-                                  icon: const Icon(
-                                    Icons.edit,
-                                    size: 32,
+                                  const Spacer(),
+                                  IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        currentInformation =
+                                            informations[index];
+                                        hashCodeCurrentInformation =
+                                            informations[index].hashCode;
+                                      });
+                                    },
+                                    icon: const Icon(
+                                      Icons.edit,
+                                      size: 32,
+                                    ),
                                   ),
-                                ),
-                                IconButton(
-                                  onPressed: () {
-                                    showDialog(
-                                      context: context,
-                                      builder: (BuildContext context) =>
-                                          AlertDialog(
-                                        title: const Text("Removendo..."),
-                                        content: const Text(
-                                            "Deseja remover essa informação?"),
-                                        actions: [
-                                          TextButton(
-                                            onPressed: () {
-                                              informations
-                                                  .remove(informations[index]);
-                                              Navigator.pop(context);
-                                            },
-                                            child: const Text('Sim'),
-                                          ),
-                                          TextButton(
-                                            onPressed: () =>
-                                                Navigator.pop(context),
-                                            child: const Text('Não'),
-                                          ),
-                                        ],
-                                      ),
-                                    );
-                                  },
-                                  icon: Icon(
-                                    Icons.cancel,
-                                    color: Colors.red[800],
-                                    size: 32,
+                                  IconButton(
+                                    onPressed: () {
+                                      showDialog(
+                                        context: context,
+                                        builder: (BuildContext context) =>
+                                            AlertDialog(
+                                          title: const Text("Removendo..."),
+                                          content: const Text(
+                                              "Deseja remover essa informação?"),
+                                          actions: [
+                                            TextButton(
+                                              onPressed: () {
+                                                informations.remove(
+                                                    informations[index]);
+                                                Navigator.pop(context);
+                                                setState(() {});
+                                              },
+                                              child: const Text('Sim'),
+                                            ),
+                                            TextButton(
+                                              onPressed: () =>
+                                                  Navigator.pop(context),
+                                              child: const Text('Não'),
+                                            ),
+                                          ],
+                                        ),
+                                      );
+                                    },
+                                    icon: Icon(
+                                      Icons.cancel,
+                                      color: Colors.red[800],
+                                      size: 32,
+                                    ),
                                   ),
-                                ),
-                              ],
-                            ),
-                            const Divider(thickness: 0.5),
-                            Text(
-                              informations[index],
-                              style: const TextStyle(
-                                color: Colors.black,
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold,
+                                ],
                               ),
-                            ),
-                          ],
+                              const Divider(thickness: 0.5),
+                              Text(
+                                informations[index],
+                                style: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                    );
+                      );
+                    },
+                  ),
+                ),
+              ),
+              Flexible(
+                child: CustomTextFormField(
+                  controller: TextEditingController(text: currentInformation),
+                  textAlign: TextAlign.center,
+                  autofocus: true,
+                  hintText: 'Digite o seu texto',
+                  validator: (value) {
+                    if (value == "") {
+                      return 'Campo obrigatório!';
+                    }
+
+                    return null;
+                  },
+                  onFieldSubmitted: (value) {
+                    if (formKey.currentState!.validate()) {
+                      if (hashCodeCurrentInformation != null) {
+                        informations = informations.map((information) {
+                          if (information.hashCode ==
+                              hashCodeCurrentInformation) {
+                            return currentInformation;
+                          }
+                          return information;
+                        }).toList();
+                      } else {
+                        informations.add(currentInformation);
+                      }
+
+                      currentInformation = '';
+                      hashCodeCurrentInformation = null;
+
+                      setState(() {});
+                    }
                   },
                 ),
               ),
-              CustomTextFormField(
-                controller: TextEditingController(text: currentInformation),
-                textAlign: TextAlign.center,
-                autofocus: true,
-                hintText: 'Digite o seu texto',
-                validator: (value) {
-                  if (value == "") {
-                    return 'Campo obrigatório!';
-                  }
-
-                  return null;
-                },
-                onFieldSubmitted: (value) {
-                  if (formKey.currentState!.validate()) {
-                    if (hashCodeCurrentInformation != null) {
-                      informations = informations.map((information) {
-                        if (information.hashCode ==
-                            hashCodeCurrentInformation) {
-                          return currentInformation;
-                        }
-                        return information;
-                      }).toList();
-                    } else {
-                      informations.add(currentInformation);
-                    }
-
-                    currentInformation = '';
-                    hashCodeCurrentInformation = null;
-
-                    setState(() {});
-                  }
-                },
-              ),
               const Spacer(),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 20),
-                child: GestureDetector(
-                  onTap: () =>
-                      launchUrl(Uri.parse("https://www.google.com.br/")),
-                  child: const Text(
-                    "Política de Privacidade",
+              Flexible(
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 20),
+                  child: GestureDetector(
+                    onTap: () =>
+                        launchUrl(Uri.parse("https://www.google.com.br/")),
+                    child: const Text(
+                      "Política de Privacidade",
+                    ),
                   ),
                 ),
               ),
